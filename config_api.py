612ffb57dda5751754341068ef36e6df50b13f86
Loup Peluso <loup.peluso@gmail.com>
2018-02-13 16:05:48 +0100

    Added FIXGUID for config Changed interface commands Fixed logger

--- config_api.py
+++ config_api.py
@@ -1,7 +1,11 @@
 import requests
 import json
+import logger
 
-class ConfigApi:
+def is_success(result):
+    return result.status_code >= 200 and result.status_code < 300
+
+class ConfigApi(logger.Logger):
     def __init__(self, url, login, password):
         self.url = url
         self.login = login
@@ -11,12 +15,25 @@ class ConfigApi:
         login_request = requests.get(self.url + '/login', auth=(self.login, self.password))
         return login_request.json()
 
+
     def get_config(self, file_output=None):
         r = requests.get(self.url + '/config', headers={'Token': self.get_token()})
+        if (not is_success(r)):
+            self.error(f'Export failure')
+            print(r.json())
         json_result = r.json()
         json_txt = json.dumps(json_result, ensure_ascii=False, indent=4)
         if file_output:
             with open(file_output, 'w', encoding='utf8') as fsock:
                 fsock.write(json_txt)
+                self.ok(f'Application exported to {file_output}')
         else:
             print(json_txt)
+
+    def fixguid(self):
+        r = requests.post(self.url + '/fixguid', headers={'Token': self.get_token()})
+        if is_success(r):
+            self.ok(f'GUID fixed (http status {r.status_code})')
+        else:
+            self.error(f'Problem occured while fixing GUID (http status {r.status_code}), please check logs')
+            
--- logger.py
+++ logger.py
@@ -6,5 +6,5 @@ class Logger:
     def ok(self, message):
         print(f'[  {c.Fore.GREEN}OK{c.Fore.RESET}  ] {c.Fore.BLUE}({self.__class__.__name__}){c.Fore.RESET}\t{message}')
     def error(self, message):
-        print(f'[  {c.Fore.RED}"ERROR"{c.Fore.RESET}  ] {c.Fore.BLUE}({self.__class__.__name__}){c.Fore.RESET}\t{message}')
+        print(f'[  {c.Fore.RED}ERROR{c.Fore.RESET}  ] {c.Fore.BLUE}({self.__class__.__name__}){c.Fore.RESET}\t{message}')
         
